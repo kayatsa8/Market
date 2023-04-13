a1461fa54c6a504a1f21a69a50869ced14e9d587
travkaie <tomer.ravkaie@intel.com>
2023-04-13 21:21:07 +0300

    PR check

--- src/main.java
+++ src/main.java
@@ -4,7 +4,7 @@ import CLI.MainMenu;
 public class main {
 
     public static void main(String[] args) {
-        new MainMenu(new Market()).run();
+        new MainMenu(new Market()).run();//main
     }
 
 }
