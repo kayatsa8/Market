f5198d1fa82faa9106d3305362fbdd8bfc0c6a0d
maliahamir <amirmal@post.bgu.ac.il>
2023-05-10 02:52:27 +0300

    Implemented discounts and discounts policies

---
+++ src/main/java/BusinessLayer/Stores/Policies/Discounts/DiscountScopes/DiscountsScopes.java
@@ -0,0 +1,7 @@
+package BusinessLayer.Stores.Policies.Discounts.DiscountScopes;
+
+public enum DiscountsScopes {
+    CATEGORY,
+    ITEMS,
+    STORE
+}
---
+++ src/main/java/BusinessLayer/Stores/Policies/Discounts/DiscountsTypes/DiscountsTypes.java
@@ -0,0 +1,7 @@
+package BusinessLayer.Stores.Policies.Discounts.DiscountsTypes;
+
+public enum DiscountsTypes {
+    VISIBLE,
+    CONDITIONAL,
+    HIDDEN
+}
