3cc74eed333104dfa80f7899eab9adf667e56d37
kayatsa <kayatsa8@gmail.com>
2023-05-11 21:47:03 +0300

    Cart & Basket support coupons.

--- src/main/java/BusinessLayer/CartAndBasket/Basket.java
+++ src/main/java/BusinessLayer/CartAndBasket/Basket.java
@@ -30,9 +30,8 @@ public class Basket {
         items.putIfAbsent(item.getItemID(), new ItemWrapper(item, quantity));
 
         releaseItems();
-        List<CartItemInfo> tempBasketItems = getItemsInfo();
-        store.updateBasket(tempBasketItems, coupons);
-        //updateBasketByCartItemInfoList(tempBasketItems);
+
+        updateBasketWithCoupons(coupons);
     }
 
     public void changeItemQuantity(int itemID, int quantity, List<String> coupons) throws Exception {
@@ -41,9 +40,8 @@ public class Basket {
         items.get(itemID).info.setAmount(quantity);
 
         releaseItems();
-        List<CartItemInfo> tempBasketItems = getItemsInfo();
-        store.updateBasket(tempBasketItems, coupons);
-        //updateBasketByCartItemInfoList(tempBasketItems);
+
+        updateBasketWithCoupons(coupons);
     }
 
     public void removeItem(int itemID, List<String> coupons) throws Exception {
@@ -55,9 +53,8 @@ public class Basket {
         items.remove(itemID);
 
         releaseItems();
-        List<CartItemInfo> tempBasketItems = getItemsInfo();
-        store.updateBasket(tempBasketItems, coupons);
-        //updateBasketByCartItemInfoList(tempBasketItems);
+
+        updateBasketWithCoupons(coupons);
     }
 
     private void validateAddItem(CatalogItem item, int quantity) throws Exception {
@@ -197,7 +194,17 @@ public class Basket {
         return items.containsKey(itemID);
     }
 
+    public void updateBasketWithCoupons(List<String> coupons){
+        List<CartItemInfo> updatedBasketItems = getItemsInfo();
+        store.updateBasket(updatedBasketItems, coupons);
+        updateBasketByCartItemInfoList(updatedBasketItems);
+    }
 
+    public void updateBasketByCartItemInfoList(List<CartItemInfo> updatedBasketItems){
+        for(CartItemInfo info : updatedBasketItems){
+            items.get(info.getItemID()).info = info;
+        }
+    }
 
 
 
