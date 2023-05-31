eb53ce9c70ba6f6d7b4eae50a7c292967d89e2f2
Yonatan Diga <digayona@post.bgu.ac.il>
2023-05-31 13:42:28 +0300

    added external systems

--- src/main/java/BusinessLayer/ExternalSystems/Supply/SupplyClient.java
+++ src/main/java/BusinessLayer/ExternalSystems/Supply/SupplyClient.java
@@ -1,22 +1,41 @@
 package BusinessLayer.ExternalSystems.Supply;
 
+import BusinessLayer.ExternalSystems.Supply.ProxyAdapter.SupplyRequestAdapter;
+import BusinessLayer.ExternalSystems.Supply.WebAdapter.SupplyAdpaterToWeb;
+
 public class SupplyClient {
 
 
 
-    private SupplyRequestAdapter adapter;
+    private SupplyRequestAdapter proxyAdapter;
+    private SupplyAdpaterToWeb webAdapter;
 
     public SupplyClient(){
-        adapter = new SupplyRequestAdapter();
+        proxyAdapter = new SupplyRequestAdapter();
+        webAdapter = new SupplyAdpaterToWeb();
     }
 
+
+    /** Old Version! delete when refactoring */
     public boolean supply(int userId, String address){
-        if(adapter != null){
-            return adapter.supply(userId, address);
+        if(proxyAdapter != null){
+            return proxyAdapter.supply(userId, address);
         }
         return false;
     }
 
     public void chooseService(){}
+
+
+    /**
+     * Version 3
+     */
+    public int supply(String buyerName, String address, String city, String country, String zip) throws Exception {
+        return webAdapter.supply(buyerName, address, city, country, zip);
+    }
+
+    public boolean cancelSupply(int transactionId) throws Exception {
+        return webAdapter.cancelSupply(transactionId);
+    }
 
 }
