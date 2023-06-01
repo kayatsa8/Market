049ed2ecc80bb219c8c6be69a18c5ed4ec378e43
kayatsa <kayatsa8@gmail.com>
2023-06-01 17:02:39 +0300

    Hibernate package first prototype, including ConnectorConfigurations and DBConnector.

---
+++ src/main/java/DataAccessLayer/Hibernate/DBConnector.java
@@ -0,0 +1,210 @@
+package DataAccessLayer.Hibernate;
+
+import javax.persistence.*;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class DBConnector<T>{
+
+    private final Class<T> clazz;
+    private EntityManager manager;
+    private ConnectorConfigurations configurations;
+    private Map<String, String> persistenceMap = new HashMap<String, String>();
+
+
+    public DBConnector(Class<T> c, ConnectorConfigurations conf){
+        clazz = c;
+        manager = null;
+        configurations = conf;
+
+        persistenceMap.put("javax.persistence.jdbc.url", configurations.getUrl());
+        persistenceMap.put("javax.persistence.jdbc.user", configurations.getUsername());
+        persistenceMap.put("javax.persistence.jdbc.password", configurations.getPassword());
+        persistenceMap.put("javax.persistence.jdbc.driver", configurations.getDriver());
+    }
+
+    /**
+     * start should be called before each non-basic query.
+     */
+    public void start(){
+        EntityManagerFactory factory = Persistence.createEntityManagerFactory(configurations.getUnitName(), persistenceMap);
+        manager = factory.createEntityManager();
+    }
+
+    public EntityManager getManager(){
+        return manager;
+    }
+
+
+    /** Basic Query **/
+    public void insert(T object){
+        start();
+
+        manager.getTransaction().begin();
+
+        manager.persist(object);
+
+        manager.getTransaction().commit();
+
+        manager.close();
+    }
+
+    /** Basic Query **/
+    public T getById(int _id){
+        start();
+
+        String stringQuery = "SELECT x " +
+                             "FROM " + clazz.getName() + " x " +
+                             "WHERE x.id = :_id";
+
+        TypedQuery<T> query = manager.createQuery(stringQuery, clazz);
+        query.setParameter("_id", _id);
+
+        T object = singleValueQuery(query);
+
+        return object;
+    }
+
+    /** Basic Query **/
+    public List<T> getAll(){
+        start();
+
+        String queryString = "SELECT x " +
+                             "FROM " + clazz.getName() + " x ";
+
+        TypedQuery<T> query = manager.createQuery(queryString, clazz);
+
+        List<T> all = manyValuesQuery(query);
+
+        return all;
+    }
+
+    /** Basic Query
+     * @param attributeName the name of the attribute in the Class
+     */
+    public void update(int _id, int newValue, String attributeName){
+        start();
+
+        String queryString = "UPDATE " + clazz.getName() + " x " +
+                             "SET x." + attributeName + " = :newValue " +
+                             "WHERE x.id = :_id";
+
+        Query query = manager.createQuery(queryString);
+        query.setParameter("newValue", newValue);
+        query.setParameter("_id", _id);
+
+        noValueQuery(query);
+    }
+
+    /** Basic Query
+     * @param attributeName the name of the attribute in the Class
+     */
+    public void update(int _id, String newValue, String attributeName){
+        start();
+
+        String queryString = "UPDATE " + clazz.getName() + " x " +
+                "SET x." + attributeName + " = :newValue " +
+                "WHERE x.id = :_id";
+
+        Query query = manager.createQuery(queryString);
+        query.setParameter("newValue", newValue);
+        query.setParameter("_id", _id);
+
+        noValueQuery(query);
+    }
+
+    /** Basic Query **/
+    public void delete(int _id){
+        start();
+
+        String queryString = "DELETE FROM " + clazz.getName() + " x " +
+                             "WHERE x.id = :_id";
+
+        Query query = manager.createQuery(queryString);
+        query.setParameter("_id", _id);
+
+        noValueQuery(query);
+    }
+
+    /** Basic Query **/
+    public void emptyTable(){
+        start();
+
+        String queryString = "DELETE FROM " + clazz.getName() + " x";
+
+        Query query = manager.createQuery(queryString);
+
+        noValueQuery(query);
+    }
+
+
+
+
+
+    // General Queries
+    /**
+     * @param query a query for a single value
+     * @return the requested value
+     */
+    public T singleValueQuery(TypedQuery<T> query){
+        T object = null;
+
+        try{
+            object = query.getSingleResult();
+        }
+        catch(NoResultException e){
+            System.out.println("\nERROR!");
+            System.out.println(e.getMessage());
+        }
+        finally{
+            manager.close();
+        }
+
+        return object;
+    }
+
+    /**
+     * @param query a query for a list of values
+     * @return the list of values in the db
+     */
+    public List<T> manyValuesQuery(TypedQuery<T> query){
+        List<T> list = null;
+
+        try{
+            list = query.getResultList();
+        }
+        catch(NoResultException e){
+            System.out.println("\nERROR!");
+            System.out.println(e.getMessage());
+        }
+        finally{
+            manager.close();
+        }
+
+        return list;
+    }
+
+    /**
+     * @param query a query that does not expect return value
+     */
+    public void noValueQuery(Query query){
+        EntityTransaction et = null;
+
+        try{
+            et = manager.getTransaction();
+            et.begin();
+            query.executeUpdate();
+            et.commit();
+        }
+        catch(NoResultException e){
+            System.out.println("\nERROR!");
+            System.out.println(e.getMessage());
+        }
+        finally{
+            manager.close();
+        }
+    }
+
+
+}
