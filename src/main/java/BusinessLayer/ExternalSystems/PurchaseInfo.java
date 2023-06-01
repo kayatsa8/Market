cd6cca70a13426654ffaa333bee2d21b5844c89f
Yonatan Diga <digayona@post.bgu.ac.il>
2023-06-01 16:58:25 +0300

    added age to purchase info

--- src/main/java/BusinessLayer/ExternalSystems/PurchaseInfo.java
+++ src/main/java/BusinessLayer/ExternalSystems/PurchaseInfo.java
@@ -1,5 +1,8 @@
 package BusinessLayer.ExternalSystems;
 
+import java.time.LocalDate;
+import java.time.Period;
+
 public class PurchaseInfo {
 
     private String cardNumber;
@@ -8,15 +11,16 @@ public class PurchaseInfo {
     private String holderName;
     private int ccv;
     private int buyerId;
+    private LocalDate birthday;
 
-    public PurchaseInfo(String cardNumber, int month, int year, String holderName, int ccv, int buyerId){
+    public PurchaseInfo(String cardNumber, int month, int year, String holderName, int ccv, int buyerId, LocalDate birthday){
         this.ccv = ccv;
         this.holderName = holderName;
         this.buyerId = buyerId;
         this.cardNumber = cardNumber;
         this.month = month;
         this.year = year;
-
+        this.birthday = birthday;
     }
 
     public String getCardNumber() {
@@ -41,5 +45,11 @@ public class PurchaseInfo {
 
     public int getBuyerId() {
         return buyerId;
+    }
+
+    public int getAge(){
+        LocalDate now = LocalDate.now();
+        Period period = Period.between(now, birthday);
+        return period.getYears();
     }
 }
