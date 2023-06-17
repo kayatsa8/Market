648f088be7f75be94f1cff8fb3c7e8ed2d8b33dc
travkaie <tomer.ravkaie@intel.com>
2023-06-17 16:22:01 +0300

    add try catch for loader

--- src/main/java/PresentationLayer/Application.java
+++ src/main/java/PresentationLayer/Application.java
@@ -3,7 +3,10 @@ package PresentationLayer;
 import BusinessLayer.ExternalSystems.PurchaseInfo;
 import BusinessLayer.ExternalSystems.SupplyInfo;
 import BusinessLayer.Market;
+import BusinessLayer.Stores.Store;
+import BusinessLayer.Users.RegisteredUser;
 import DataAccessLayer.Hibernate.ConnectorConfigurations;
+import DataAccessLayer.Hibernate.DBConnector;
 import PresentationLayer.initialize.ConfigReader;
 import PresentationLayer.initialize.Loader;
 import ServiceLayer.Objects.CatalogItemService;
@@ -17,6 +20,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import java.time.LocalDate;
+import java.util.List;
+import java.util.logging.ConsoleHandler;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -34,59 +39,19 @@ public class Application implements AppShellConfigurator {
     private static final String relativePath = "src/main/java/PresentationLayer/initialize/data.json";
 
     public static void main(String[] args) throws Exception {
-        String addressOk="addressOk";
-        LocalDate bDayOk=LocalDate.of(2022, 7, 11);
-
         ConfigReader configReader=new ConfigReader();
-        String relativePath = configReader.getInitializePath();
+        DBConnector<Store> connector = new DBConnector<>(Store.class, readDBConfigurations(configReader));
 
-//        try{
-//            readDBConfigurations(configReader);
-//        }
-//        catch(Exception e){
-//            System.out.println("ERROR: unable to load the DB configurations to the system!");
-//            System.exit(1);
-//        }
-
-        Loader loader=new Loader();
-        loader.load(relativePath);
-
-        /*try {
-            ShoppingService shoppingService = new ShoppingService();
-            UserService userService = new UserService();
-            int amirID = userService.register("Amir", "amirsPass",addressOk,bDayOk).getValue();
-            int tomerID = userService.register("Tomer", "tomersPass",addressOk,bDayOk).getValue();
-            int yonatanID = userService.register("Yonatan", "yonantansPass",addressOk,bDayOk).getValue();
-            int sagiID = userService.register("Sagi", "sagisPass",addressOk,bDayOk).getValue();
-            int meitarID = userService.register("Meitar", "meitarsPass",addressOk,bDayOk).getValue();
-            int maorID = userService.register("Maor", "maorsPass",addressOk,bDayOk).getValue();
-
-            userService.login("Amir", "amirsPass");
-            userService.login("Maor", "maorsPass");
-
-            int amirStoreID = shoppingService.createStore(amirID, "amirs Store").getValue();
-            int maorStoreID = shoppingService.createStore(maorID, "maors Store").getValue();
-
-            userService.addOwner(amirID, tomerID, amirStoreID);
-            userService.addManager(amirID, meitarID, amirStoreID);
-            Result<CatalogItemService> result = shoppingService.addItemToStore(amirStoreID, "Bread", 5, "Wheat", 50);
-            shoppingService.addItemToStore(amirStoreID, "Milk", 6, "Dairy", 10);
-            shoppingService.addItemToStore(amirStoreID, "Butter", 7, "Dairy", 30);
-            int itemId = result.getValue().getItemID();
-            shoppingService.addItemAmount(amirStoreID, itemId, 50);
-            shoppingService.addItemAmount(amirStoreID, itemId + 1, 30);
-            shoppingService.addItemAmount(amirStoreID, itemId + 2, 5);
-
-            shoppingService.addItemToCart(maorID, amirStoreID, itemId, 5);
-            shoppingService.buyCart(maorID, getPurchaseInfo(), getSupplyInfo());
-
-            userService.logout(amirID);
-            userService.logout(maorID);
+        List<Store> stores = connector.getAll();
+        if (stores.size()>0) {
+            System.out.println("The system already has stores, not loading from loader.");
         }
-        catch (Exception e) {
-            System.out.println("Problem initiating Market");
-            return;
-        }*/
+        else {
+            String relativePath = configReader.getInitializePath();
+
+            Loader loader = new Loader();
+            loader.load(relativePath);
+        }
 
         SpringApplication.run(Application.class, args);
     }
@@ -100,15 +65,14 @@ public class Application implements AppShellConfigurator {
         return new SupplyInfo("Name", "address", "city", "counyrt", "asd");
     }
 
-    public static void readDBConfigurations(ConfigReader configReader) throws Exception {
+    public static ConnectorConfigurations readDBConfigurations(ConfigReader configReader) throws Exception {
         String name = configReader.getDBName();
         String url = configReader.getDBUrl();
         String username = configReader.getDBUsername();
         String password = configReader.getDBPassword();
         String driver = configReader.getDBDriver();
-        ConnectorConfigurations conf = new ConnectorConfigurations(name, url, username, password, driver);
 
-        Market.getInstance().setConfigurations(conf);
+        return new ConnectorConfigurations(name, url, username, password, driver);
     }
 
 }
